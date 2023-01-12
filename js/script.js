e5362cd2828a3e8ec8f3bbc6712edc3451371179
pogozh <pogozh@gmail.com>
2023-01-12 22:29:24 +0600

    123

--- js/script.js
+++ js/script.js
@@ -1,113 +1,152 @@
 "use strict";
 
-let personalMovieDB = {
-  count: 0,
-  movies: {},
-  actors: {},
-  genres: [],
-  private: false,
+// let personalMovieDB = {
+//   count: 0,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   private: false,
 
-  start: function () {
-    this.count = '';
-    while (this.count == '' || this.count === null || isNaN(this.count)) {
-      this.count = +prompt('How many movies did You watch?', '');
-    }
-  },
+//   start: function () {
+//     this.count = '';
+//     while (this.count == '' || this.count === null || isNaN(this.count)) {
+//       this.count = +prompt('How many movies did You watch?', '');
+//     }
+//   },
 
-  writeYourGenres: () => {
-    for (let i = 0; i < 2; ++i) {
-      let genre = ``;
-      while (genre === '' || genre == null || typeof (genre) !== 'string') {
-        console.log(`Incorrct input\n`);
-        genre = prompt(`Ваш любимый жанр под номером ${i + 1}`, '');
-      }
-      personalMovieDB.genres[i] = genre.trim();
-    }
+//   writeYourGenres: () => {
+//     for (let i = 0; i < 2; ++i) {
+//       let genre = ``;
+//       while (genre === '' || genre == null || typeof (genre) !== 'string') {
+//         console.log(`Incorrct input\n`);
+//         genre = prompt(`Ваш любимый жанр под номером ${i + 1}`, '');
+//       }
+//       personalMovieDB.genres[i] = genre.trim();
+//     }
 
-    personalMovieDB.genres.forEach((item, i) => {
-      console.log(`Любимый жанр № ${i + 1} - это ${item}`);
-    });
-  },
+//     personalMovieDB.genres.forEach((item, i) => {
+//       console.log(`Любимый жанр № ${i + 1} - это ${item}`);
+//     });
+//   },
 
-  writeAllGenries: () => {
-    let genres = ``;
-    while (genres === '' || genres == null || typeof (genres) !== 'string') {
-      genres = prompt(`Введите любимые жанры  через запятую`, '').toLowerCase();
-    }
+//   writeAllGenries: () => {
+//     let genres = ``;
+//     while (genres === '' || genres == null || typeof (genres) !== 'string') {
+//       genres = prompt(`Введите любимые жанры  через запятую`, '').toLowerCase();
+//     }
 
-    personalMovieDB.genres = genres.split(`, `);
-    personalMovieDB.genres.sort();
+//     personalMovieDB.genres = genres.split(`, `);
+//     personalMovieDB.genres.sort();
 
-    personalMovieDB.genres.forEach((item, i) => {
-      console.log(`Любимый жанр № ${i + 1} - это ${item}`);
-    });
-  },
+//     personalMovieDB.genres.forEach((item, i) => {
+//       console.log(`Любимый жанр № ${i + 1} - это ${item}`);
+//     });
+//   },
 
-  showMyDB: function (hidden) {
-    if (!hidden) {
-      console.log(`showing DB:\n`, personalMovieDB);
-    }
-  },
+//   showMyDB: function (hidden) {
+//     if (!hidden) {
+//       console.log(`showing DB:\n`, personalMovieDB);
+//     }
+//   },
 
-  rememberMyFilms: function () {
-    let movie = '',
-      score = -1,
-      messege = '';
-    for (let i = 0; i < 2; ++i) {
-      movie = '';
-      score = -1;
-      messege = (i + 1) + '-й из просмотренных фильмов?';
-      while (movie === null || movie === '' || movie.length > 50) {
-        movie = prompt(messege, '').trim();
-      }
-      while (score == null || isNaN(score) || typeof (score) !== 'number' || score < 1 || score > 10) {
-        score = +prompt('Ha сколько оцените ero?', '');
-      }
+//   rememberMyFilms: function () {
+//     let movie = '',
+//       score = -1,
+//       messege = '';
+//     for (let i = 0; i < 2; ++i) {
+//       movie = '';
+//       score = -1;
+//       messege = (i + 1) + '-й из просмотренных фильмов?';
+//       while (movie === null || movie === '' || movie.length > 50) {
+//         movie = prompt(messege, '').trim();
+//       }
+//       while (score == null || isNaN(score) || typeof (score) !== 'number' || score < 1 || score > 10) {
+//         score = +prompt('Ha сколько оцените ero?', '');
+//       }
 
-      personalMovieDB.movies[movie] = score;
-    }
-  },
+//       personalMovieDB.movies[movie] = score;
+//     }
+//   },
 
-  detectPersonalLevel: function () {
-    console.log('count - ', this.count);
-    if (this.count < 10) {
-      console.log(`You don't watch a lot of movies (${this.count}).`);
-      alert(`You don't watch a lot of movies (${this.count}).`);
-    } else {
-      console.log(`you watch too many movies (${this.count})!`);
-      alert(`You watch too many movies (${this.count})!`);
-    }
-  },
+//   detectPersonalLevel: function () {
+//     console.log('count - ', this.count);
+//     if (this.count < 10) {
+//       console.log(`You don't watch a lot of movies (${this.count}).`);
+//       alert(`You don't watch a lot of movies (${this.count}).`);
+//     } else {
+//       console.log(`you watch too many movies (${this.count})!`);
+//       alert(`You watch too many movies (${this.count})!`);
+//     }
+//   },
 
-  toggleVisibleMyDB: function () {
-    this.private = !(this.private);
-  },
+//   toggleVisibleMyDB: function () {
+//     this.private = !(this.private);
+//   },
 
+// };
+
+// // personalMovieDB.start();
+// // personalMovieDB.writeYourGenres();
+// // personalMovieDB.rememberMyFilms();
+// // personalMovieDB.detectPersonalLevel();
+// // console.log(personalMovieDB.private);
+// // personalMovieDB.showMyDB(personalMovieDB.private);
+// // personalMovieDB.toggleVisibleMyDB();
+
+
+
+// const str = `4,2,7,11,67,5,--,a,z,g`;
+// // prompt('str->?array','');
+// let arr = str.split(`,`);
+// // debagger;
+// arr = arr.sort(comareNumbers);
+// console.log(arr.join(`;`));
+
+// function comareNumbers(a, b) {
+//   return a - b;
+// }
+
+// let duhovka = {
+//   deep: 55,
+//   wide: 57,
+//   high: 58,
+// };
+
+// ---S2P45--
+
+const butn = document.querySelectorAll(`button`);
+const overlay = document.querySelector(`.overlay`);
+const deleteElement = (e) => {
+  console.log(e.target);
+  console.log(e.type);
+  console.log(e.currentTarget);
+  // ++i;
+  // if (i == 2) { butn.removeEventListener(`click`, deleteElement); }
 };
 
-// personalMovieDB.start();
-// personalMovieDB.writeYourGenres();
-// personalMovieDB.rememberMyFilms();
-// personalMovieDB.detectPersonalLevel();
-// console.log(personalMovieDB.private);
-// personalMovieDB.showMyDB(personalMovieDB.private);
-// personalMovieDB.toggleVisibleMyDB();
+butn.forEach(btn => {
+  btn.addEventListener(`click`, deleteElement, {once: true});
+});
+// console.log(butn);
+// butn.onclick = function () { alert(`44444444`); };
 
+// butn.addEventListener(`click`, ()=>{
+//   alert(`555`);
+// });
 
+// let i = 0;
 
-const str = `4,2,7,11,67,5,--,a,z,g`;
-// prompt('str->?array','');
-let arr = str.split(`,`);
-debagger;
-arr = arr.sort(comareNumbers);
-console.log(arr.join(`;`));
+// butn.addEventListener(`mouseenter`, (event) => {
+//   console.log(`6666`);
+//   // event.target.remove();
+// });
 
-function comareNumbers(a, b) {
-  return a - b;
-}
+// butn[0].addEventListener(`click`, deleteElement);
+// overlay.addEventListener(`click`, deleteElement);
 
-let duhovka =  {
-deep: 55 ,
-wide: 57,
-high: 58,
-};
+const link = document.querySelector(`a`);
+
+link.addEventListener(`click`, function (event) {
+  event.preventDefault();
+  console.log(event.target);
+});
