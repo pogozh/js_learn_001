2935f838f7a332721ce048f95f5338557e73b6c5
pogozh <pogozh@gmail.com>
2023-02-08 21:56:11 +0600

    S2P48 done

---
+++ project_S2P48_MobTouch/script.js
@@ -0,0 +1,55 @@
+//touchstart
+//touchmove
+//touchend
+//touchenter
+//touchleave
+//touchcancel
+
+window.addEventListener(`DOMContentLoaded`, () => {
+    const box = document.querySelector(`.box`);
+
+
+    box.addEventListener(`touchstart`, (event) => {
+        event.preventDefault();
+
+        console.log(`start`);
+        console.log(`touches`, event.touches);
+        console.log(`targettouches`, event.touches);
+
+    });
+
+    box.addEventListener(`touchmove`, (event) => {
+        event.preventDefault();
+
+        console.log(`move`, event.targetTouches[0].pageX);
+    });
+
+    box.addEventListener(`touchend`, (event) => {
+        event.preventDefault();
+
+        console.log(`end`);
+    });
+
+    box.addEventListener(`touchenter`, (event) => {
+        event.preventDefault();
+
+        console.log(`enter`);
+    });
+
+    box.addEventListener(`touchleav`, (event) => {
+        event.preventDefault();
+
+        console.log(`leav`);
+    });
+
+    box.addEventListener(`touchcancel`, (event) => {
+        event.preventDefault();
+
+        console.log(`cancel`);
+    });
+});
+
+// touches
+// targetTouches
+// changetTouches
+
