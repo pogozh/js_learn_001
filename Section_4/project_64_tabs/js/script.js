3dbfab4beb59a6a1de45501a4827c40dc6fe4f2f
pogozh <pogozh@gmail.com>
2023-10-09 22:34:58 +0600

    updated:

--- Section_4/project_64_tabs/js/script.js
+++ Section_4/project_64_tabs/js/script.js
@@ -1,8 +1,15 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-  const tabs = document.querySelectorAll('.tabheader__items');
+  const tabs = document.querySelectorAll('.tabheader__item');
   const tabsContent = document.querySelectorAll('.tabcontent');
   const tabsParent = document.querySelector('.tabheader__items');
+
+  let i = 0;
+  tabs.forEach(tab => {
+    console.log('tabsContent', i, tabsContent[i]);
+    console.log('Tabs[', i, ']', tabs[i]);
+    ++i;
+  });
 
   function hideTabContent() {
     tabsContent.forEach(item => {
@@ -15,12 +22,11 @@ window.addEventListener('DOMContentLoaded', () => {
 
   function showTabContent(i = 0) {
     tabsContent[i].style.display = 'block';
-    console.log('->>', i, tabsContent[i]);
     tabs[i].classList.add('tabheader__item_active');
   }
 
   hideTabContent();
-  showTabContent(1);
+  showTabContent();
 
   tabsParent.addEventListener('click', function (event) {
     const target = event.target;
